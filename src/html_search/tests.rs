5ae0252e59fca528c96fb2e1b19a9b1b09e98914
Rodney Lab <codesign@rodneylab.com>
2023-03-22 18:37:16 +0000

    style: 🛁 linting

--- src/html_search/tests.rs
+++ src/html_search/tests.rs
@@ -42,18 +42,6 @@ fn search_html_highlight_highlights_nested_matches() {
         "apple",
     )
     .to_string();
-    //     let result = search_html(
-    // r#"<p>If you have followed me for a while, you will know I am a huge vanilla-extract
-    // fan. It has a lot of the features of Tailwind and in fact many of the benefits
-    // of CSS in JS frameworks. However, in my opinion, two factors make it worth
-    // considering vanilla-extract as an alternative: <strong>TypeScript</strong> and <strong>zero-runtime
-    // overhead</strong>. You write your styles in TypeScript, so get the in-editor
-    // <strong><abbr>DX<tool-tip inert="" role="tooltip">Developer Experience</tool-tip></abbr>
-    // benefits</strong> of Intellisense, checking and completion. The bonus is that the
-    // tooling (works with <strong>Vite</strong>, <strong>ESBuild</strong> and WebPack) transpiles the styles,
-    // and you only ship CSS to the browser — <strong>no
-    // <abbr>UX<tool-tip inert="" role="tooltip">User Experience</tool-tip></abbr>
-    // performance&nbsp;hit</strong>.</p>"#,"TypeScript").to_string();
 
     let expected = r#"<h2>Heading</h2><p>Nobody likes maple in their <strong><mark id="search-match">apple</mark></strong> flavoured Sn<mark>apple</mark>. <mark>APPLE</mark></p><p>Paragraph with no matches</p><p>Paragraph which mentions <mark>apple</mark>s again</p>"#;
     assert_eq!(result, expected);
